5b77f8c6e73ca49288aa4f8ccc1959b9966c37bd
frieswithsalsa <romansedin@hotmail.com>
2024-08-25 12:46:22 +0200

    step 4 completed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/frieswithsalsa/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/frieswithsalsa/frontend-project-46/actions)
+
+## Сравнение плоских файлов
+[![asciicast](https://asciinema.org/a/94JvCjpMoy9N81DkBqHf8YKlL.svg)](https://asciinema.org/a/94JvCjpMoy9N81DkBqHf8YKlL)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import path from "path";
 import fs from "fs";
 import parse from "./parse.js";
-// import buildTree from "./treeBuilder.js"
-
+import _ from "lodash";
 
 const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 const extractFormat = (filepath) => path.extname(filepath).slice(1);
-const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), extractFormat(filepath))
+const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), extractFormat(filepath));
 
 const gendiff = (filepath1, filepath2) => {
   const fullFilePath1 = getFullPath(filepath1);
@@ -15,7 +14,22 @@ const gendiff = (filepath1, filepath2) => {
   const data1 = getData(fullFilePath1);
   const data2 = getData(fullFilePath2);
 
-  // format(tree, formatName);
-}
+  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
 
-export default gendiff
+  const result = keys.map((key) => {
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+    }
+    return `    ${key}: ${data1[key]}`;
+  });
+
+  return `{\n${result.join('\n')}\n}`;
+};
+
+export default gendiff;
