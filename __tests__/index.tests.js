3b581b5104b2013565a7a97de0238744d496aced
frieswithsalsa <romansedin@hotmail.com>
2024-08-29 16:14:22 +0200

    added test

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,1 +1,25 @@
-import gendiff from "../src/index.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const expectedOutput = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+describe('gendiff', () => {
+  test('Correct two plain files comparison', () => {
+    const file1 = path.resolve(__dirname, '../__fixtures__/file1.json');
+    const file2 = path.resolve(__dirname, '../__fixtures__/file2.json');
+
+    const result = gendiff(file1, file2);
+    expect(result.trim()).toEqual(expectedOutput.trim());
+  });
+});
