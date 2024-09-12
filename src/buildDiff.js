0a1b690ed23411025dc9ed15a2cff4409f9a0f1c
frieswithsalsa <romansedin@hotmail.com>
2024-09-12 18:50:22 +0200

    fixes

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,21 +1,17 @@
 const buildDiff = (data1, data2) => {
-  const keys = [...new Set([...Object.keys(data1), ...Object.keys(data2)])]
-    .slice()
-    .sort((a, b) => a.localeCompare(b));
+  const keys = [...new Set([...Object.keys(data1), ...Object.keys(data2)])];
+  const sortedKeys = [...keys].sort((a, b) => a.localeCompare(b));
 
-  return keys.map((key) => {
+  return sortedKeys.map((key) => {
     if (!Object.hasOwn(data1, key)) {
       return { key, type: 'added', value: data2[key] };
     }
     if (!Object.hasOwn(data2, key)) {
       return { key, type: 'deleted', value: data1[key] };
     }
-    if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
-      return {
-        key,
-        type: 'nested',
-        children: buildDiff(data1[key], data2[key]),
-      };
+    if (typeof data1[key] === 'object' && data1[key] !== null &&
+        typeof data2[key] === 'object' && data2[key] !== null) {
+      return { key, type: 'nested', children: buildDiff(data1[key], data2[key]) };
     }
     if (data1[key] !== data2[key]) {
       return {
