5a270f24289d85df1c4a44c6d46aa20a4eb1b375
frieswithsalsa <romansedin@hotmail.com>
2024-09-24 16:49:37 +0200

    some fixes

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -6,8 +6,10 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('genDiff', () => {
   test('JSON files stylish format', () => {
@@ -21,7 +23,9 @@ describe('genDiff', () => {
     console.log('Result:');
     console.log(result);
     console.log('Difference:');
-    console.log(expected === result ? 'No difference' : 'Strings are different');
+    console.log(
+      expected === result ? 'No difference' : 'Strings are different'
+    );
 
     expect(result).toBe(expected);
   });
