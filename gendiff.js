cdcfa9660fb4d4395a1550daa10f4c08e5d39a0b
frieswithsalsa <romansedin@hotmail.com>
2024-12-02 18:10:26 +0200

    Little changes in description

--- gendiff.js
+++ gendiff.js
@@ -4,7 +4,7 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference')
+    .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
 
 program.parse(process.argv);
