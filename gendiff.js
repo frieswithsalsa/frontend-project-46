e559f6b01c0b05e5e8a89ac3a20ca94d61da7023
frieswithsalsa <romansedin@hotmail.com>
2024-12-02 18:28:21 +0200

    Added one new option and new argument

--- gendiff.js
+++ gendiff.js
@@ -6,5 +6,6 @@ const program = new Command();
 program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
-
+    .option('-f, --format [type]', 'output format' )
+    .argument('<filepath1> <filepath2>')
 program.parse(process.argv);
