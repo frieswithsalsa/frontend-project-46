af9081e61a32235d7f663b02da47be32bbd82a63
frieswithsalsa <romansedin@hotmail.com>
2025-01-10 12:01:55 +0200

    rename to camelCase

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { readFileSync } from 'fs';
 import { resolve, extname } from 'path';
 import { cwd } from 'process';
-import parseFile from './parsers.js';
+import parse from './parsers.js';
 import buildDiff from './buildDiff.js';
 import formatter from './formatters/index.js';
 
-const readfile = (filepath) => {
+const readFile = (filepath) => {
   const currentDir = cwd();
   const absolutePath = resolve(currentDir, filepath);
   const content = readFileSync(absolutePath, 'utf-8');
@@ -15,8 +15,8 @@ const readfile = (filepath) => {
 const getExtension = (file) => extname(file).slice(1);
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const file1 = parseFile(getExtension(filepath1), readfile(filepath1));
-  const file2 = parseFile(getExtension(filepath2), readfile(filepath2));
+  const file1 = parse(getExtension(filepath1), readFile(filepath1));
+  const file2 = parse(getExtension(filepath2), readFile(filepath2));
   const diffTree = buildDiff(file1, file2);
   return formatter(diffTree, format);
 };
